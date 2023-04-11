e892656a695fccd50361e4d02488a6f4028a90de
baudeliocf <baudeliocf@gmail.com>
2023-04-11 12:23:59 -0700

    Se realizo cambios de datos de usuario PUT

--- src/main/java/com/ecommerce/service/UsuarioService.java
+++ src/main/java/com/ecommerce/service/UsuarioService.java
@@ -33,25 +33,62 @@ public class UsuarioService {
 	}
 
 	public Usuario loginUsuario(String email, String password) {
-		return usuarioRepository.findByEmailAndPassword(email, password).orElseThrow(() -> new IllegalStateException("Usuario o contraseña incorrectos"));
+		return usuarioRepository.findByEmailAndPassword(email, password)
+				.orElseThrow(() -> new IllegalStateException("Usuario o contraseña incorrectos"));
 	}
 
 	public Usuario addUsuario(Usuario usuario) {
-        Optional<Usuario> usuarioByName = usuarioRepository.findByEmail(usuario.getEmail());
-        if (usuarioByName.isPresent()) {
+		Optional<Usuario> usuarioByName = usuarioRepository.findByEmail(usuario.getEmail());
+		if (usuarioByName.isPresent()) {
 			return new Usuario();
-            // throw new IllegalStateException("El correo " + usuario.getEmail() + " ya esta registrado");
-        } else {
+			// throw new IllegalStateException("El correo " + usuario.getEmail() + " ya esta
+			// registrado");
+		} else {
 			return usuarioRepository.save(usuario);
 		}
-    }
+	}
 
 	// Put
 	// Busca el elemento para actualizarlo si no lo encuentra lo crea
 	public Usuario updateUsuario(Usuario newUsuario, Long id) {
 		return usuarioRepository.findById(id)
 				.map(usuario -> {
-					usuario.setNombre(newUsuario.getNombre());
+					if (newUsuario.getNombre() != null && !newUsuario.getNombre().isEmpty()) {
+						usuario.setNombre(newUsuario.getNombre());
+					}
+					if (newUsuario.getApellido() != null && !newUsuario.getApellido().isEmpty()) {
+						usuario.setApellido(newUsuario.getApellido());
+					}
+					if (newUsuario.getEmail() != null && !newUsuario.getEmail().isEmpty()) {
+						usuario.setEmail(newUsuario.getEmail());
+					}
+					if (newUsuario.getPassword() != null && !newUsuario.getPassword().isEmpty()) {
+						usuario.setPassword(newUsuario.getPassword());
+					}
+					if (newUsuario.getTelefono() != null && !newUsuario.getTelefono().isEmpty()) {
+						usuario.setTelefono(newUsuario.getTelefono());
+					}
+					if (newUsuario.getNacimiento() != null && !newUsuario.getNacimiento().isEmpty()) {
+						usuario.setNacimiento(newUsuario.getNacimiento());
+					}
+					if (newUsuario.getCalle() != null && !newUsuario.getCalle().isEmpty()) {
+						usuario.setCalle(newUsuario.getCalle());
+					}
+					if (newUsuario.getNumero() != null && !newUsuario.getNumero().isEmpty()) {
+						usuario.setNumero(newUsuario.getNumero());
+					}
+					if (newUsuario.getColonia() != null && !newUsuario.getColonia().isEmpty()) {
+						usuario.setColonia(newUsuario.getColonia());
+					}
+					if (newUsuario.getMunicipio() != null && !newUsuario.getMunicipio().isEmpty()) {
+						usuario.setMunicipio(newUsuario.getMunicipio());
+					}
+					if (newUsuario.getEstado() != null && !newUsuario.getEstado().isEmpty()) {
+						usuario.setEstado(newUsuario.getEstado());
+					}
+					if (newUsuario.getCodigopostal() != null && !newUsuario.getCodigopostal().isEmpty()) {
+						usuario.setCodigopostal(newUsuario.getCodigopostal());
+					}
 					return usuarioRepository.save(usuario);
 				})
 				.orElseGet(() -> {
@@ -59,8 +96,8 @@ public class UsuarioService {
 				});
 	}
 
-	//Actualiza algunos campos del elemento?
-	//Transactional es como un commit si alfo falla regresa a su estado inicial
+	// Actualiza algunos campos del elemento?
+	// Transactional es como un commit si alfo falla regresa a su estado inicial
 	@Transactional
 	public void updateUsuario(Long id, String name, String info) {
 		Usuario usuario = usuarioRepository.findById(id)
